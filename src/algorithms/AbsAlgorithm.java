17e324ea5198a75da78aeb5a5ccf2b477e9e0903
jarqprog <jarqprog@gmail.com>
2018-02-15 09:31:22 +0100

    add asbtract class AbsAlgorithm, Factorial class, refactor

---
+++ src/algorithms/AbsAlgorithm.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+import java.math.BigInteger;
+
+public abstract class AbsAlgorithm implements Algorithm {
+
+    protected String name;
+
+    public abstract BigInteger executeAlgorithm(int num);
+
+    public String toString() {
+        return "algorithm: " + name;
+    }
+
+}
