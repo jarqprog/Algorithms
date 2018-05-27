2b021e86787cbff669a6ff5df08fd4b00dac673f
jarqprog <jarqprog@gmail.com>
2018-05-27 22:22:10 +0200

    sort refactor, implement generoc method, add tests

--- src/main/java/com/jarq/sorts/Bubble.java
+++ src/main/java/com/jarq/sorts/Bubble.java
@@ -6,11 +6,43 @@ public class Bubble implements Sorting {
 
     @Override
     public <T extends Comparable<T>> List<T> sort(List<T> elements) {
-        return null;
+        int len = elements.size();
+        T temp;
+
+        for(int i=0; i<len; i++) {
+            for(int j=1; j<len; j++) {
+
+                T current = elements.get(j);
+                T prev = elements.get(j-1);
+
+                if(current.compareTo(prev) < 0) {
+                    temp = prev;
+                    elements.set(j-1, current);
+                    elements.set(j, temp);
+                }
+            }
+        }
+        return elements;
     }
 
     @Override
     public int[] sort(int[] elements) {
-        return new int[0];
+        int len = elements.length;
+        int temp;
+
+        for(int i=0; i<len; i++) {
+            for(int j=1; j<len; j++) {
+
+                int current = elements[j];
+                int prev = elements[j-1];
+
+                if(current < prev) {
+                    temp = prev;
+                    elements[j-1] = current;
+                    elements[j] = temp;
+                }
+            }
+        }
+        return elements;
     }
 }
--- src/main/java/com/jarq/sorts/Insertion.java
+++ src/main/java/com/jarq/sorts/Insertion.java
@@ -8,11 +8,43 @@ public class Insertion implements Sorting {
 
     @Override
     public <T extends Comparable<T>> List<T> sort(List<T> elements) {
-        return null;
+        int len = elements.size();
+        T temp, current, previous;
+
+        for(int i=1; i<len; i++) {
+            current = elements.get(i);
+            previous = elements.get(i-1);
+            if(current.compareTo(previous) < 0) {
+                for(int j=i; j>0; j--) {
+                    current = elements.get(j);
+                    previous = elements.get(j-1);
+                    if(current.compareTo(previous) < 0) {
+                        temp = previous;
+                        elements.set(j-1, current);
+                        elements.set(j, temp);
+                    }
+                }
+            }
+        }
+        return elements;
     }
 
     @Override
     public int[] sort(int[] elements) {
-        return new int[0];
+        int len = elements.length;
+        int temp;
+
+        for(int i=1; i<len; i++) {
+            if(elements[i] < elements[i-1]) {
+                for(int j=i; j>0; j--) {
+                    if(elements[j] < elements[j-1]) {
+                        temp = elements[j-1];
+                        elements[j-1] = elements[j];
+                        elements[j] = temp;
+                    }
+                }
+            }
+        }
+        return elements;
     }
 }
