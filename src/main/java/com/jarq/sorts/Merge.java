8d33fdeff89c679b65d650677e31b3cd69f6c39a
jarqprog <jarqprog@gmail.com>
2018-05-27 23:35:20 +0200

    implement merge sort with test

--- src/main/java/com/jarq/sorts/Merge.java
+++ src/main/java/com/jarq/sorts/Merge.java
@@ -1,16 +1,106 @@
 package com.jarq.sorts;
 
-import java.util.List;
+import java.util.*;
 
 public class Merge implements Sorting {
 
     @Override
     public <T extends Comparable<T>> List<T> sort(List<T> elements) {
-        return null;
+        int size = elements.size();
+        if(size < 2) {
+            return elements;
+        }
+
+        int middle = size/ 2;
+        List<T> left = sort(elements.subList(0, middle));
+        List<T> right = sort(elements.subList(middle, size));
+        return merge(left, right);
     }
 
     @Override
     public int[] sort(int[] elements) {
-        return new int[0];
+        int size = elements.length;
+        if(size < 2) {
+            return elements;
+        }
+
+        int middle = size/ 2;
+        int[] left = sort(Arrays.copyOfRange(elements,0, middle));
+        int[] right = sort(Arrays.copyOfRange(elements,middle, size));
+        return merge(left, right);
+    }
+
+
+    private int[] merge(int[] left, int[] right) {
+
+        int leftIndex = 0;
+        int leftSize = left.length;
+
+        int rightIndex = 0;
+        int rightSize = right.length;
+
+        int[] result = new int[leftSize + rightSize];
+        int resultIndex = 0;
+
+        while(leftIndex < leftSize && rightIndex < rightSize) {
+
+            if(left[leftIndex] < right[rightIndex]) {
+                result[resultIndex] = left[leftIndex];
+                leftIndex++;
+            } else {
+                result[resultIndex] = right[rightIndex];
+                rightIndex++;
+            }
+            resultIndex++;
+        }
+
+        while(leftIndex < leftSize) {
+            result[resultIndex] = left[leftIndex];
+            leftIndex++;
+            resultIndex++;
+        }
+
+        while(rightIndex < rightSize) {
+            result[resultIndex] = right[rightIndex];
+            rightIndex++;
+            resultIndex++;
+        }
+        return result;
+    }
+
+    private <T extends Comparable<T>> List<T> merge(List<T> left, List<T> right) {
+
+        int leftIndex = 0;
+        int leftSize = left.size();
+
+        int rightIndex = 0;
+        int rightSize = right.size();
+
+        List<T> result = new LinkedList<>();
+
+        while(leftIndex < leftSize && rightIndex < rightSize) {
+
+            T currentLeft = left.get(leftIndex);
+            T currentRight = right.get(rightIndex);
+
+            if(currentLeft.compareTo(currentRight) < 0) {
+                result.add(currentLeft);
+                leftIndex++;
+            } else {
+                result.add(currentRight);
+                rightIndex++;
+            }
+        }
+
+        while(leftIndex < leftSize) {
+            result.add(left.get(leftIndex));
+            leftIndex++;
+        }
+
+        while(rightIndex < rightSize) {
+            result.add(right.get(rightIndex));
+            rightIndex++;
+        }
+        return result;
     }
 }
---
+++ src/test/java/com/jarq/sorts/MergeTest.java
@@ -0,0 +1,31 @@
+package com.jarq.sorts;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MergeTest {
+
+    private Sorting sorting = new Merge();
+
+    @Test
+    public void sort_list() {
+
+        List<Integer> unsorted = Arrays.asList(4, -11, 8, 2000, -3, 1, 0);
+        List<Integer> sorted = Arrays.asList(-11, -3, 0, 1, 4, 8, 2000);
+
+        assertEquals(sorted, sorting.sort(unsorted));
+    }
+
+    @Test
+    public void sort_array() {
+
+        int[] unsorted = {4, -11, 8, 2000, -3, 1, 2};
+        int[] sorted = {-11, -3, 1, 2, 4, 8, 2000};
+
+        assertArrayEquals(sorted, sorting.sort(unsorted));
+    }
+}
